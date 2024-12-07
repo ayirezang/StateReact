6ec477438bc08252c9e361de5d832b48a50c83fb
MAC <mac@MACs-MacBook.local>
2024-12-07 12:47:30 -0800

    react-class-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import Form from "./Component/form";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
-
-    setName("");
-    setEmail("");
-    setPassword("");
-  };
-
   return (
-    <div className="h-screen flex justify-center items-center ">
-      <form
-        className="flex flex-col justify-center items-center gap-4"
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          placeholder="Name"
-          className="input input-bordered w-full max-w-xs"
-          onChange={handleNameChange}
-          value={name}
-        />
-        <input
-          type="Email"
-          placeholder="Email"
-          className="input input-bordered w-full max-w-xs"
-          onChange={handleEmailChange}
-          value={email}
-        />
-        <input
-          type="Password"
-          placeholder="Password"
-          className="input input-bordered w-full max-w-xs"
-          onChange={handlePasswordChange}
-          value={password}
-        />
-        <button type="submit" className="btn btn-accent w-full">
-          Submit
-        </button>
-      </form>
+    <div>
+      <Form />
     </div>
   );
 };
